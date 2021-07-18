43daf57fa4cc9e93805766b94eff36ca094f5500
Balaje K <balaje6@gmail.com>
2021-07-18 18:47:39 +1000

    Minor fixes for manifolds and add test

--- src/CellData/CellFields.jl
+++ src/CellData/CellFields.jl
@@ -263,7 +263,7 @@ function _point_to_cell_cache(trian::Triangulation)
   ctype_to_reffe = get_reffes(trian)
   ctype_to_polytope = map(get_polytope, ctype_to_reffe)
   cell_map = get_cell_map(trian)
-  cell_normal = (num_pt_dims ≥ 3) ? get_data(get_normal_vector(trian)) : cell_map # To return something else
+  cell_normal = get_data(get_normal_vector(trian)) # To return something else
   cell_coords = get_cell_coordinates(trian)
   cache1 = kdtree, vertex_to_cells, cell_to_ctype, ctype_to_polytope, cell_map, cell_normal, cell_coords, num_pt_dims, D
 end
@@ -294,7 +294,7 @@ function _point_to_cell!(cache, x::Point)
     inv_cmap = inverse_map(cmap)
 
     # For manifolds
-    if(num_pt_dims ≥  D && num_pt_dims ≥ 3)
+    if(num_pt_dims ≥  D)
       # Check for 3D distance
       n = evaluate(cell_normal[cell], inv_cmap(x));
       p₀ = cell_coords[cell][1] # Point on the cell
@@ -304,7 +304,6 @@ function _point_to_cell!(cache, x::Point)
     else
       return distance(polytope, inv_cmap, x)
     end
-
   end
   # findmin, without allocating an array
   cell = zero(eltype(cells))
--- test/CellDataTests/CellFieldsTests.jl
+++ test/CellDataTests/CellFieldsTests.jl
@@ -278,6 +278,60 @@ Random.seed!(0)
     end
 end
 
+@testset "evaluating functions on manifolds" for D in 2:4
+    # Create domain
+    xmin=0
+    xmax=1
+
+    domain = repeat([xmin,xmax], D)
+    ncells = 3
+    partition = repeat([ncells], D)
+
+    order = 2
+    reffe = ReferenceFE(lagrangian, Float64, order)
+
+    model = CartesianDiscreteModel(domain, partition)
+    bgmodel = simplexify(model)
+    labels = get_face_labeling(bgmodel)
+    bgface_to_mask = get_face_mask(labels, "boundary", D-1) # Get the boundary of the bgmodel
+    bmodel = BoundaryDiscreteModel(Polytope{D-1}, bgmodel, bgface_to_mask)
+
+    coeff0 = rand(Float64)
+    coeffs = rand(SVector{D, Float64})
+    f(x) = coeffs ⋅ SVector(Tuple(x)) + coeff0
+    V = FESpace(bmodel, reffe)
+    fh = interpolate_everywhere(f, V)
+
+    xs = [VectorValue(rand(D-1)..., xmax) for i in 1:10]
+    ys = [VectorValue(xmin, rand(D-1)...) for i in 1:10]
+    for i in 1:length(xs)
+      x = xs[i]
+      fx = f(x)
+      fhx = evaluate(fh, x)
+      @test fx ≈ fhx
+
+      y = ys[i]
+      fy = f(y)
+      fhy = evaluate(fh, y)
+      @test fy ≈ fhy
+    end
+
+    fhxs = fh.(xs)
+    fhys = fh.(ys)
+    @test fhxs ≈ f.(xs)
+    @test fhys ≈ f.(ys)
+
+    # Random points outside bmodel, but inside model
+    pt = [VectorValue(rand(D)) for i in 1:5]
+    try
+      fh.(pt)
+    catch
+      @test 1==1
+    end
+
+end
+
+
 #np = 3
 #ndofs = 4
 #
